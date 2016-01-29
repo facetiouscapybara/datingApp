73899648a81aee3a0a9d596786776898673e0f38
Brian Sweeney <bsweeney930@gmail.com>
2016-01-28 17:18:57 -0800

    added mens waiting room. changed some component names to camel case

---
+++ datingApp/src/components/mensWaitingRoom.js
@@ -0,0 +1,16 @@
+//essentailly a splash page for when men have no chat requests
+import React, { Componenet, View, Text, StyleSheet}
+
+export default class Matches extends Component {
+	render () {
+		return (
+
+		)
+	}
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex:1
+	}
+})
