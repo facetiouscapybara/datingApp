e0e57c71cda8ee1eab99bec1438c60ca653af313
Brian Sweeney <bsweeney930@gmail.com>
2016-01-30 14:28:23 -0800

    scrolling now working in List.js

--- datingApp/src/components/list.js
+++ datingApp/src/components/list.js
@@ -1,5 +1,5 @@
 //this will be the page the females see when they first log in showing them who is around them
-import React, { Component, View, Text, StyleSheet, ListView} from 'react-native';
+import React, { Component, View, Text, StyleSheet, ListView, Image, ScrollView} from 'react-native';
 import ListItem from './listItem'
 
 
@@ -9,13 +9,24 @@ export default class List extends Component {
 	render () {
 		console.log("I'm trying")
 		return (
-      <View style={styles.container}>
-      	<Text> 
+			<View style={styles.container}>
+				<Text> 
       		Dude's nearby
       	</Text>
-      	<ListItem user={users[0]} />
+				<ScrollView
+	        automaticallyAdjustContentInsets={true}
+	        style={styles.scrollView}>
+	      	{this.users()}
+      	</ScrollView>
       </View>
 		)
+	}
+
+	users () {
+		var userList = dudes.map(function(user){
+			return <ListItem user={user} key={user.fbID} style={styles.listItem}/>
+		})
+		return userList
 	}
 };
 
@@ -24,17 +35,21 @@ const styles = StyleSheet.create({
 		marginTop: 20,
 		flex:1,
 		backgroundColor: 'green'
+	},
+	scrollView: {
+		height: 300
 	}
 })
 
-const users = [
+const dudes = [
 	{
-		name: 'brian',
+		name: 'Brian Sweeney',
 		age: 24,
 		description: 'Moustache Rides Anyone?',
 		preference: 'female',
 		gender: 'male',
-		imageUrl:'http://images-cdn.moviepilot.com/images/c_fill,h_331,w_500/t_mp_quality/xel5asph5jw8z2fw6xmc/super-troopers-2-crowd-funded-in-24-hours-324417.jpg'
+		imageUrl:'http://images-cdn.moviepilot.com/images/c_fill,h_331,w_500/t_mp_quality/xel5asph5jw8z2fw6xmc/super-troopers-2-crowd-funded-in-24-hours-324417.jpg',
+		fbID: 1234
 	},
 	{
 		name: 'Eric Geneisse',
@@ -42,7 +57,17 @@ const users = [
 		description: 'Need some woodworking done?',
 		preference: 'female',
 		gender: 'male',
-		imageUrl:'https://www.google.com/search?q=google+images&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjT_7qZndLKAhVP0GMKHexpAgoQ_AUIBygB&biw=1274&bih=778#tbm=isch&q=super+troopers&imgrc=m1xF8_5_7tLPAM%3A'
+		imageUrl:'http://media4.popsugar-assets.com/files/2010/02/08/2/192/1922283/cop-slide9/i/Rabbit-Thorny-Super-Troopers.jpg',
+		fbID: 12345
+	},
+	{
+		name: 'Dan Frehner',
+		age: 29,
+		description: 'Sup Nerds?!?!?',
+		preference: 'female',
+		gender: 'male',
+		imageUrl:'http://cache.boston.com/bonzai-fba/Original_Photo/2006/09/12/1158092070_4142.jpg',
+		fbID: 123456
 	},
 	{
 		name: 'Zelong Ma',
@@ -50,14 +75,43 @@ const users = [
 		description: 'My name is Marlon and I\'m here to party',
 		preference: 'female',
 		gender: 'male',
-		imageUrl:'http://content.internetvideoarchive.com/content/photos/583/620523_013.jpg'
+		imageUrl:'http://content.internetvideoarchive.com/content/photos/583/620523_013.jpg',
+		fbID: 1234567
 	},
 	{
-		name: 'Lucky Lady',
-		age: 26,
+		name: 'Brian Sweeney',
+		age: 24,
 		description: 'Moustache Rides Anyone?',
-		preference: 'male',
-		gender: 'female',
-		imageUrl:'http://www.psdgraphics.com/file/female-silhouette.jpg'
+		preference: 'female',
+		gender: 'male',
+		imageUrl:'http://images-cdn.moviepilot.com/images/c_fill,h_331,w_500/t_mp_quality/xel5asph5jw8z2fw6xmc/super-troopers-2-crowd-funded-in-24-hours-324417.jpg',
+		fbID: 1233244
+	},
+	{
+		name: 'Eric Geneisse',
+		age: 28,
+		description: 'Need some woodworking done?',
+		preference: 'female',
+		gender: 'male',
+		imageUrl:'http://media4.popsugar-assets.com/files/2010/02/08/2/192/1922283/cop-slide9/i/Rabbit-Thorny-Super-Troopers.jpg',
+		fbID: 1234512342142
+	},
+	{
+		name: 'Dan Frehner',
+		age: 29,
+		description: 'Sup Nerds?!?!?',
+		preference: 'female',
+		gender: 'male',
+		imageUrl:'http://cache.boston.com/bonzai-fba/Original_Photo/2006/09/12/1158092070_4142.jpg',
+		fbID: 123456123412432143
+	},
+	{
+		name: 'Zelong Ma',
+		age: 23,
+		description: 'My name is Marlon and I\'m here to party',
+		preference: 'female',
+		gender: 'male',
+		imageUrl:'http://content.internetvideoarchive.com/content/photos/583/620523_013.jpg',
+		fbID: 123452354367
 	}
 ]
--- datingApp/src/components/listItem.js
+++ datingApp/src/components/listItem.js
@@ -1,17 +1,38 @@
-import React, { Component, View, Text, StyleSheet} from 'react-native';
+import React, { Component, View, Text, StyleSheet, Image, ScrollView} from 'react-native';
 
 export default class ListItem extends Component {
 	render () {
 		return (
-      <Text>
-      	{this.props.user.name}
-      </Text>
+			
+			<View style={styles.container}>
+				<Image 
+					source={{uri: this.props.user.imageUrl}}
+        	style={styles.image} />
+        <View>
+		      <Text>
+		      	{this.props.user.name}, {this.props.user.age}
+		      </Text>
+		      <Text>
+		      	{this.props.user.description}
+		      </Text>
+		  	</View>
+	    </View>
 		)
 	}
 };
 
 const styles = StyleSheet.create({
 	container: {
-		flex:1
+		borderWidth: 2,
+		borderColor: 'black',
+		marginTop: 10,
+		padding: 5
+	},
+	image : {
+		width: 150, 
+		height: 150
+	},
+	textBody:{
+	
 	}
 })
