2fd0915088c83260f3044a758b78ed118a3defcd
Brian Sweeney <bsweeney930@gmail.com>
2016-02-01 17:25:56 -0800

    editProfile styling, and get request to db for initial data.

--- datingApp/src/components/editProfile.js
+++ datingApp/src/components/editProfile.js
@@ -1,27 +1,23 @@
 //edit user Profile
-import React, { Component, View, Text, StyleSheet, TextInput} from 'react-native';
+import React, { Component, View, Text, StyleSheet, TextInput, Image} from 'react-native';
 import host from './../../constants.js'
+import GameChanger from '../../ios/somehowFixesEverything.gif';
+
+let that;
+
 export default class Matches extends Component {
 
 		constructor(props){
 			super(props)
+
+			that = this;
 
 			this.state = {
 				text: ""
 			}
 		}
 
-	// constructor () {
-	// 	super();
-
-	// 	this.onInputChange = this.onInputChange.bind(this);
-
-	// 	return {
-	// 			text: ""
-	// 		}
-	// }
-
-	componentWillMount (props) {
+	componentDidMount (props) {
 		let urlPath = host.SERVER_URL + '/api/users/' + this.props.profile.id
 		console.log(urlPath)
 		//need to set authorization header
@@ -31,13 +27,13 @@ export default class Matches extends Component {
 			headers: {
 				'Accept': 'application/json',
         'Content-Type': 'application/json',
-				'Authorization': 'CAAOXApvBWf4BADK8rwcr4LB7oFtZBPvIKUOSB1aaSUNxDTHdOiNzCb9xkqi8nWxsNhoaCkEy42QI8C95KbZChrGbmZCK12K2T9vbNmyUhnNt0a9Wvw3BYEpac4tXj7eW70kyXqDFrGMKloc7py7xmzwKhWfgogGQHykpHQcMEiRouJFmkteNF1BRhhkZCHdbZCHIuwUJOtpHtdHzbuQ9fwPwfyGizsewZD'
+				'Authorization': 'Bearer CAAOXApvBWf4BADK8rwcr4LB7oFtZBPvIKUOSB1aaSUNxDTHdOiNzCb9xkqi8nWxsNhoaCkEy42QI8C95KbZChrGbmZCK12K2T9vbNmyUhnNt0a9Wvw3BYEpac4tXj7eW70kyXqDFrGMKloc7py7xmzwKhWfgogGQHykpHQcMEiRouJFmkteNF1BRhhkZCHdbZCHIuwUJOtpHtdHzbuQ9fwPwfyGizsewZD'
 			}
 		}
 		fetch(urlPath, queryObject)
 			.then(function(res){
-				console.log(res)
-				this.setState({
+				result = JSON.parse(res._bodyText)
+				that.setState({
 					id: result.id,
           access_token: result.access_token,
           first_name : result.first_name,
@@ -50,31 +46,35 @@ export default class Matches extends Component {
           headline: result.headline 
 				})
 			})
-	}
-
-	onInputChange (event) {
-		this.setState({
-			text: event.target.value
-		})
+			.catch(function(error){
+				console.log(typeof this.state.picture)
+			})
+			.then(function(res){
+				console.log(res);
+			})
 	}
 
 	render () {
 		return (
       <View style={styles.container}>
+        <Image style={styles.image} source={{uri: this.state.picture}} />
       	<Text> Headline: </Text>
       	<TextInput
-    			style={styles.textInput}
+    			style={styles.headline}
     			onChangeText={(text) => this.setState({text})}//also must be changed to reset headline
     			value={this.state.text}//eventualy this.state.headline
+    			maxLength={100}
   			/>
   			<Text></Text>
   			<Text></Text>
 
   			<Text> Bio: </Text>
       	<TextInput
-    			style={styles.textInput}
+    			style={styles.bio}
     			onChangeText={(text) => this.setState({text})}//changed to reset bio
     			value={this.state.text}//this.state.bio
+    			multiline= {true}
+    			maxLength={500}
   			/>
       </View>
 		)
@@ -86,11 +86,30 @@ const styles = StyleSheet.create({
 		marginTop: 65,
 		flex:1
 	},
-	textInput: {
+	headline: {
+		paddingLeft: 5,
 		height: 40, 
 		borderColor: 'gray', 
 		borderWidth: 1,
 		marginRight: 50,
-		marginLeft: 30
-	}
+		marginLeft: 30,
+		borderRadius: 5
+	},
+	bio: {
+		paddingLeft: 5,
+		height: 200, 
+		borderColor: 'gray', 
+		borderWidth: 1,
+		marginRight: 50,
+		marginLeft: 30,
+		borderRadius: 5
+
+	},
+	image: {
+		height: 125,
+		width: 125,
+		borderRadius: 65,
+		marginTop: 50,
+		alignSelf: 'center'
+	},
 })
