4e1a8027ce3d6dcc9de247ebf95a662bea46a828
Brian Sweeney <bsweeney930@gmail.com>
2016-02-01 16:19:14 -0800

    deleted some unneeded files, added headline field to db, began work on editProfile Page

--- datingApp/src/components/editProfile.js
+++ datingApp/src/components/editProfile.js
@@ -1,18 +1,81 @@
 //edit user Profile
 import React, { Component, View, Text, StyleSheet, TextInput} from 'react-native';
-
+import host from './../../constants.js'
 export default class Matches extends Component {
+
+		constructor(props){
+			super(props)
+
+			this.state = {
+				text: ""
+			}
+		}
+
+	// constructor () {
+	// 	super();
+
+	// 	this.onInputChange = this.onInputChange.bind(this);
+
+	// 	return {
+	// 			text: ""
+	// 		}
+	// }
+
+	componentWillMount (props) {
+		let urlPath = host.SERVER_URL + '/api/users/' + this.props.profile.id
+		console.log(urlPath)
+		//need to set authorization header
+		let accessToken = JSON.stringify(this.props.access_token)
+		let queryObject = {
+			method: "GET",
+			headers: {
+				'Accept': 'application/json',
+        'Content-Type': 'application/json',
+				'Authorization': 'CAAOXApvBWf4BADK8rwcr4LB7oFtZBPvIKUOSB1aaSUNxDTHdOiNzCb9xkqi8nWxsNhoaCkEy42QI8C95KbZChrGbmZCK12K2T9vbNmyUhnNt0a9Wvw3BYEpac4tXj7eW70kyXqDFrGMKloc7py7xmzwKhWfgogGQHykpHQcMEiRouJFmkteNF1BRhhkZCHdbZCHIuwUJOtpHtdHzbuQ9fwPwfyGizsewZD'
+			}
+		}
+		fetch(urlPath, queryObject)
+			.then(function(res){
+				console.log(res)
+				this.setState({
+					id: result.id,
+          access_token: result.access_token,
+          first_name : result.first_name,
+          name: result.name,
+          age: result.age || 'null',
+          picture: result.picture,
+          gender: result.gender,
+          preference: result.preference,
+          bio: result.bio,
+          headline: result.headline 
+				})
+			})
+	}
+
+	onInputChange (event) {
+		this.setState({
+			text: event.target.value
+		})
+	}
+
 	render () {
 		return (
       <View style={styles.container}>
+      	<Text> Headline: </Text>
       	<TextInput
     			style={styles.textInput}
-    			onChangeText={(text) => this.setState({text})}
-    			value=""
-  />
+    			onChangeText={(text) => this.setState({text})}//also must be changed to reset headline
+    			value={this.state.text}//eventualy this.state.headline
+  			/>
+  			<Text></Text>
+  			<Text></Text>
 
-
-      	
+  			<Text> Bio: </Text>
+      	<TextInput
+    			style={styles.textInput}
+    			onChangeText={(text) => this.setState({text})}//changed to reset bio
+    			value={this.state.text}//this.state.bio
+  			/>
       </View>
 		)
 	}
@@ -26,6 +89,8 @@ const styles = StyleSheet.create({
 	textInput: {
 		height: 40, 
 		borderColor: 'gray', 
-		borderWidth: 1
+		borderWidth: 1,
+		marginRight: 50,
+		marginLeft: 30
 	}
 })
