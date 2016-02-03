875a084499693aab11c1cb9967bfc077fe78ab7d
Eric Geniesse <egeniesse@gmail.com>
2016-02-03 15:19:33 -0800

    took out all of the things from material ui

--- datingApp/src/main.js
+++ datingApp/src/main.js
@@ -16,7 +16,7 @@ export default class Main extends Component {
         style={styles.container} 
         initialRoute={{
           title: 'TOLO',
-          component: Chatroom,
+          component: Splash,
           navigationBarHidden: true
         }} />
     )
