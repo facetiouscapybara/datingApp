f02892b6add421884a9d41902739530fb39f1813
tetondan <dfrehner1@gmail.com>
2016-02-10 14:58:49 -0800

    commiting style changes

--- datingApp/src/components/matches.js
+++ datingApp/src/components/matches.js
@@ -297,8 +297,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
     alignItems: 'center',
-    paddingTop: 40,
-    backgroundColor: '#fff'
+    paddingTop: 40
 	},
   noMatches: {
     flex: 1,
