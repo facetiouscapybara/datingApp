906a61fce921a15b9d304559f6a8adf2279a30f5
Eric Geniesse <egeniesse@gmail.com>
2016-02-10 14:13:31 -0800

    took off express session from dependency

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,7 +2,6 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var passport = require('passport');
 var passportInit = require('./passport.js');
-var session = require('express-session');
 
 
 
@@ -10,7 +9,6 @@ module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '../../../docs/'));
   app.use(passport.initialize());
   app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
